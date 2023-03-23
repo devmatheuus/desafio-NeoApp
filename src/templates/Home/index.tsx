a9f6007a5cee39600fecb978f446d60234b17dc7
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-22 23:30:28 -0300

    feat: rendering Comics

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { ComicsContainer } from '../../Components/ComicsContainer';
 import { Header } from '../../Components/Header';
 
 export const Home: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <ComicsContainer />
+    </>
+  );
 };
