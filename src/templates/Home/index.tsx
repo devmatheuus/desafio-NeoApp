d751afd6ab438c8f8c9fba511490d74ff85e1ed7
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-25 17:04:37 -0300

    fix: using global import heder

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -4,16 +4,15 @@ import { Button } from '../../Components/Button';
 import { ComicsBox } from '../../Components/ComicsBox';
 import { ComicsContainer } from '../../Components/ComicsContainer';
 import { Footer } from '../../Components/Footer';
-import { Header } from '../../Components/Header';
 import { LoadingSpinner } from '../../Components/LoadingSpinner';
 import { useComics } from '../../contexts/ComicsContext';
 import * as Styled from './styles';
 
 export const Home: React.FC = () => {
   const { loadMoreComics, isLoading, search } = useComics();
+
   return (
     <>
-      <Header />
       <ComicsBox />
       <ComicsContainer />
       {!search && (
