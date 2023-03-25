6fd0a2facc9069b895980a43604e35bf5b07e592
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-25 17:04:53 -0300

    fix: using global header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Header } from './Components/Header';
 import { Router } from './routes';
 
 export const App: React.FC = () => (
   <>
+    <Header />
     <Router />
   </>
 );
