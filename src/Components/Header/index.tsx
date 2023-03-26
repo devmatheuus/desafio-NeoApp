743956f6e3d2afd2ed503a784ebfa8a41c2e2c0e
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 00:39:40 -0300

    feat: add navigation buttons on the header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
+import { useCart } from '../../contexts/CartContext';
 import { SearchInput } from '../SearchInput';
 import * as Styled from './styles';
 
 export const Header: React.FC = () => {
+  const { comicsInCart } = useCart();
+
+  const navigate = useNavigate();
   return (
     <Styled.HeaderContainer>
       <div>
-        <div>
+        <div role="button" onClick={() => navigate('/')}>
           <h1>MARVEL COMICS</h1>
         </div>
 
         <Styled.HeaderActionsContainer role="button">
           <SearchInput />
-          <AiOutlineShoppingCart size="2.5rem" />
+          <Styled.HeaderCartContainer
+            role="button"
+            onClick={() => navigate('/checkout')}
+          >
+            {comicsInCart.length > 0 && <span>{comicsInCart.length}</span>}
+            <AiOutlineShoppingCart size="3rem" />
+          </Styled.HeaderCartContainer>
         </Styled.HeaderActionsContainer>
       </div>
     </Styled.HeaderContainer>
