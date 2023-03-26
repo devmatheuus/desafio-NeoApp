92240849c40df99d34ca07a07af80dd6f55774be
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 16:31:18 -0300

    feat: component to manipulate quantity of items in the cart

---
+++ src/Components/ItemsOnCartCounter/index.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from 'react';
+
+import { useCart } from '../../contexts/CartContext';
+import * as Styled from './styles';
+
+interface IItemsOnCartCounter {
+  quantity: number;
+  comicId: number;
+  //   subtractQuantity: (quantity: number) => void;
+}
+
+export const ItemsOnCartCounter: React.FC<IItemsOnCartCounter> = ({
+  comicId,
+  quantity,
+}: IItemsOnCartCounter) => {
+  const { editComicQuantity } = useCart();
+
+  const subtractQuantity = useCallback(() => {
+    if (quantity > 1) {
+      editComicQuantity(comicId, quantity - 1);
+    }
+  }, [quantity, comicId, editComicQuantity]);
+
+  const addQuantity = useCallback(() => {
+    editComicQuantity(comicId, quantity + 1);
+  }, [comicId, quantity, editComicQuantity]);
+
+  return (
+    <Styled.CounterContainer title="Manipulate quantity of items">
+      <button onClick={subtractQuantity} title="Remove one item">
+        -
+      </button>
+      <span>{quantity}</span>
+      <button onClick={addQuantity} title="Add one item">
+        +
+      </button>
+    </Styled.CounterContainer>
+  );
+};
---
+++ src/Components/ItemsOnCartCounter/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const CounterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 80px;
+  gap: 1rem;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  background: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 4px;
+  padding: 0.4rem 1rem;
+
+  button {
+    border: none;
+    background: none;
+
+    color: ${({ theme }) => theme.colors.white};
+    font-size: ${({ theme }) => theme.fonts.sizes.small};
+    cursor: pointer;
+  }
+
+  span {
+    font-size: ${({ theme }) => theme.fonts.sizes.small};
+  }
+`;
