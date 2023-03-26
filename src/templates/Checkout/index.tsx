8547e5278628ddc0badb87c0e632f489a9eb1dd7
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 00:41:04 -0300

    feat: checkout page

--- src/templates/Checkout/index.tsx
+++ src/templates/Checkout/index.tsx
@@ -1,5 +1,80 @@
 import React from 'react';
 
+import { AnimatedCardImage } from '../../Components/AnimatedCardImage';
+import { Button } from '../../Components/Button';
+import { Footer } from '../../Components/Footer';
+import { Header } from '../../Components/Header';
+import * as Styled from './styles';
+
+const mock = [
+  {
+    id: 1,
+    title: 'X-Men: Phoenix - Warsong (2006)',
+    price: 3.99,
+    thumbnail: {
+      path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/50/4c3645d0d29e3',
+      extension: 'jpg',
+    },
+  },
+  {
+    id: 2,
+    title: 'X-Men: Phoenix - Warsong (2006)',
+    price: 3.99,
+    thumbnail: {
+      path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/50/4c3645d0d29e3',
+      extension: 'jpg',
+    },
+  },
+  {
+    id: 3,
+    title: 'X-Men: Phoenix - Warsong (2006)',
+    price: 3.99,
+    thumbnail: {
+      path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/50/4c3645d0d29e3',
+      extension: 'jpg',
+    },
+  },
+];
+
 export const Checkout: React.FC = () => {
-  return <h1>Checkout</h1>;
+  return (
+    <>
+      <Header />
+      <Styled.CheckoutContainer>
+        <Styled.CheckoutList>
+          {mock.map((item) => (
+            <Styled.CheckoutItem key={item.id}>
+              <AnimatedCardImage
+                thumbnail={item.thumbnail}
+                title={item.title}
+                size="portrait_xlarge"
+                height="150"
+              />
+
+              <div>
+                <h2>{item.title}</h2>
+                <h3>{item.price}</h3>
+              </div>
+            </Styled.CheckoutItem>
+          ))}
+        </Styled.CheckoutList>
+        <Styled.CheckoutInfos>
+          <Styled.CheckoutInfosDescription>
+            <h2>Total</h2>
+            <h3>Quantity</h3>
+          </Styled.CheckoutInfosDescription>
+          <Styled.CheckoutInfosPrice>
+            <h2>$ 3.99</h2>
+            <h3>3</h3>
+          </Styled.CheckoutInfosPrice>
+        </Styled.CheckoutInfos>
+        <Styled.ButtonContainer>
+          <Button text="Add discount coupon" />
+          <Button text="Purchase" />
+        </Styled.ButtonContainer>
+      </Styled.CheckoutContainer>
+
+      <Footer />
+    </>
+  );
 };
---
+++ src/templates/Checkout/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+export const CheckoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 117px);
+  max-width: 1440px;
+  margin: 1rem auto;
+  padding: 0 1rem;
+  gap: 2.5rem;
+`;
+
+export const CheckoutList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  /* gap: 1rem; */
+  width: 50%;
+  max-height: 400px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.black};
+    border-radius: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: #eee;
+  }
+`;
+
+export const CheckoutItem = styled.li`
+  display: flex;
+  gap: 3rem;
+  padding: 1rem 2rem;
+  width: 80%;
+  border-top: 1px solid #c3c3c3;
+
+  > div {
+    gap: 1rem;
+    display: flex;
+    flex-direction: column;
+
+    h2 {
+      font-size: 2rem;
+      color: ${({ theme }) => theme.colors.black};
+      font-weight: bold;
+    }
+  }
+`;
+
+export const CheckoutInfos = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  padding: 1rem 2rem;
+`;
+
+export const CheckoutInfosDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  font-size: ${({ theme }) => theme.fonts.sizes.small};
+  color: ${({ theme }) => theme.colors.black};
+  font-weight: bold;
+`;
+
+export const CheckoutInfosPrice = styled(CheckoutInfosDescription)`
+  align-items: flex-end;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 50%;
+  justify-content: space-evenly;
+
+  gap: 1rem;
+
+  > button {
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.black};
+  }
+
+  button:last-child {
+    background-color: green;
+  }
+`;
