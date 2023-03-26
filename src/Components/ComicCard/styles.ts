00e7483684aeeb1b8d03d6d9f2805f494d048da2
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-25 22:18:16 -0300

    feat: altering paragraph color on hover event

--- src/Components/ComicCard/styles.ts
+++ src/Components/ComicCard/styles.ts
@@ -8,35 +8,17 @@ export const CardContainer = styled.div`
   gap: 1.5rem;
 
   font-family: ${({ theme }) => theme.fonts.family.primary};
+  color: ${({ theme }) => theme.colors.black};
 
-  figure {
-    box-shadow: 0 26px 35px -15px rgba(0, 0, 0, 0.4);
-
-    overflow: hidden;
-    max-height: 300px;
-
-    display: flex;
-    flex-direction: column;
-
-    width: 200px;
-    gap: 2rem;
-
-    img {
-      width: 100%;
-      height: 100%;
-
-      &:hover {
-        transform: scale(1.1);
-      }
-
-      transition: 0.5s;
-    }
-  }
   p {
-    color: ${({ theme }) => theme.colors.black};
     text-align: start;
 
     font-weight: bold;
     font-size: 1.4rem;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.red};
+    transition: 0.5s;
   }
 `;
