a7033adef2dcbd1ff8142602c92330ab0ed17b1e
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 16:28:31 -0300

    feat: removing mocks

--- src/templates/Checkout/index.tsx
+++ src/templates/Checkout/index.tsx
@@ -1,44 +1,27 @@
 import React, { useState } from 'react';
+import { BsTrash3 } from 'react-icons/bs';
+import { SlBag } from 'react-icons/sl';
+import { useNavigate } from 'react-router-dom';
 
 import { AnimatedCardImage } from '../../Components/AnimatedCardImage';
 import { Button } from '../../Components/Button';
 import { Footer } from '../../Components/Footer';
 import { Header } from '../../Components/Header';
+import { ItemsOnCartCounter } from '../../Components/ItemsOnCartCounter';
 import { DiscountCouponModal } from '../../Components/Modals/DiscountCoupon';
+import { useCart } from '../../contexts/CartContext';
+import { calculateTotalCart } from '../../utils/calculateTotalCart';
+import { getQuantityOfItemsOnCart } from '../../utils/getItemsOfItensOnCart';
+import { numberFormatter } from '../../utils/numberFormmater';
 import * as Styled from './styles';
-
-const mock = [
-  {
-    id: 1,
-    title: 'X-Men: Phoenix - Warsong (2006)',
-    price: 3.99,
-    thumbnail: {
-      path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/50/4c3645d0d29e3',
-      extension: 'jpg',
-    },
-  },
-  {
-    id: 2,
-    title: 'X-Men: Phoenix - Warsong (2006)',
-    price: 3.99,
-    thumbnail: {
-      path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/50/4c3645d0d29e3',
-      extension: 'jpg',
-    },
-  },
-  {
-    id: 3,
-    title: 'X-Men: Phoenix - Warsong (2006)',
-    price: 3.99,
-    thumbnail: {
-      path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/50/4c3645d0d29e3',
-      extension: 'jpg',
-    },
-  },
-];
 
 export const Checkout: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
+  const { comicsInCart, removeFromCart } = useCart();
+
+  const itensOnCart = getQuantityOfItemsOnCart(comicsInCart);
+
+  const navigate = useNavigate();
 
   return (
     <>
@@ -49,42 +32,64 @@ export const Checkout: React.FC = () => {
         />
       )}
       <Header />
-      <Styled.CheckoutContainer>
-        <Styled.CheckoutList>
-          {mock.map((item) => (
-            <Styled.CheckoutItem key={item.id}>
-              <AnimatedCardImage
-                thumbnail={item.thumbnail}
-                title={item.title}
-                size="portrait_xlarge"
-                height="150"
-              />
 
-              <div>
-                <h2>{item.title}</h2>
-                <h3>{item.price}</h3>
-              </div>
-            </Styled.CheckoutItem>
-          ))}
-        </Styled.CheckoutList>
-        <Styled.CheckoutInfos>
-          <Styled.CheckoutInfosDescription>
-            <h2>Total</h2>
-            <h3>Quantity</h3>
-          </Styled.CheckoutInfosDescription>
-          <Styled.CheckoutInfosPrice>
-            <h2>$ 3.99</h2>
-            <h3>3</h3>
-          </Styled.CheckoutInfosPrice>
-        </Styled.CheckoutInfos>
-        <Styled.ButtonContainer>
-          <Button
-            text="Add discount coupon"
-            onClick={() => setShowModal(true)}
-          />
-          <Button text="Purchase" />
-        </Styled.ButtonContainer>
-      </Styled.CheckoutContainer>
+      {comicsInCart.length <= 0 && (
+        <Styled.NoItemsContainer>
+          <SlBag size="12rem" />
+          <p>You have no items in the cart</p>
+          <Button text="Buy now" onClick={() => navigate('/')} />
+        </Styled.NoItemsContainer>
+      )}
+
+      {comicsInCart.length > 0 && (
+        <Styled.CheckoutContainer>
+          <Styled.CheckoutList>
+            {comicsInCart.map((item) => (
+              <Styled.CheckoutItem key={item.id}>
+                <AnimatedCardImage
+                  thumbnail={item.thumbnail}
+                  title={item.title}
+                  size="portrait_xlarge"
+                  height="150"
+                />
+
+                <div>
+                  <h2>{item.title}</h2>
+                  <p>{numberFormatter(item.prices[0].price)}</p>
+                  <div>
+                    <ItemsOnCartCounter
+                      comicId={item.id}
+                      quantity={item.quantity}
+                    />
+                    <button onClick={() => removeFromCart(item.id)}>
+                      <span>Remove</span>
+                      <BsTrash3 />
+                    </button>
+                  </div>
+                </div>
+              </Styled.CheckoutItem>
+            ))}
+          </Styled.CheckoutList>
+          <Styled.CheckoutInfos>
+            <Styled.CheckoutInfosDescription>
+              <h2>Total</h2>
+              <h3>Quantity</h3>
+            </Styled.CheckoutInfosDescription>
+            <Styled.CheckoutInfosPrice>
+              <h2>{calculateTotalCart(comicsInCart)}</h2>
+              <h3>{itensOnCart}</h3>
+            </Styled.CheckoutInfosPrice>
+          </Styled.CheckoutInfos>
+          <Styled.ButtonContainer>
+            <Button
+              text="Add discount coupon"
+              onClick={() => setShowModal(true)}
+            />
+            <Button text="Purchase" />
+          </Styled.ButtonContainer>
+        </Styled.CheckoutContainer>
+      )}
+
       <Footer />
     </>
   );
