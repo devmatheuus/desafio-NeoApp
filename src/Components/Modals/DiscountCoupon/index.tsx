f0f8d9b70bfff2f9929f453a4afdaa83bd0ac710
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 16:27:08 -0300

    feat: dynamizing style

--- src/Components/Modals/DiscountCoupon/index.tsx
+++ src/Components/Modals/DiscountCoupon/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Button } from '../../Button';
-import * as Styled from './styles';
+import * as Styled from '../modalStyles';
 
 interface Props {
   onClose: () => void;
