5055934f1ed7490e1359e2a4dd26b0c2ee8ec5f0
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 16:30:05 -0300

    feat: adjusting logic of add to cart function and function to edit quantity of items in cart

--- src/contexts/CartContext/index.tsx
+++ src/contexts/CartContext/index.tsx
@@ -1,19 +1,16 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
 
-import { IComic } from '../../types/IComicData';
+import { IComic, IComicsInCart } from '../../types/IComicData';
 
 interface ICartProvider {
   children: React.ReactNode;
 }
 
-interface IComicsInCart extends IComic {
-  quantity: number;
-}
-
 interface ICartContext {
   comicsInCart: IComicsInCart[];
-  addToCart: (comic: IComic) => void;
+  addToCart: (comic: IComic & { quantity: number }) => void;
   removeFromCart: (comicId: number) => void;
+  editComicQuantity: (comicId: number, quantity: number) => void;
 }
 
 export const CartContext = createContext<ICartContext>({} as ICartContext);
@@ -28,19 +25,22 @@ export const CartProvider = ({ children }: ICartProvider) => {
   const [comicsInCart, setComicsInCart] = useState<IComicsInCart[]>([]);
 
   const addToCart = useCallback(
-    (comic: IComic) => {
+    (comic: IComic & { quantity: number }) => {
       const comicExists = comicsInCart.find((item) => item.id === comic.id);
 
       if (comicExists) {
         setComicsInCart((state) =>
           state.map((item) =>
             item.id === comic.id
-              ? { ...item, quantity: item.quantity + 1 }
+              ? { ...item, quantity: item.quantity + comic.quantity }
               : item
           )
         );
       } else {
-        setComicsInCart((state) => [...state, { ...comic, quantity: 1 }]);
+        setComicsInCart((state) => [
+          ...state,
+          { ...comic, quantity: comic.quantity },
+        ]);
       }
     },
     [comicsInCart]
@@ -50,9 +50,16 @@ export const CartProvider = ({ children }: ICartProvider) => {
     setComicsInCart((state) => state.filter((item) => item.id !== comicId));
   }, []);
 
+  const editComicQuantity = useCallback((comicId: number, quantity: number) => {
+    setComicsInCart((state) =>
+      state.map((item) => (item.id === comicId ? { ...item, quantity } : item))
+    );
+  }, []);
+
   return (
     <CartContext.Provider
       value={{
+        editComicQuantity,
         addToCart,
         comicsInCart,
         removeFromCart,
