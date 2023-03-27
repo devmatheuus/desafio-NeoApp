34024db41d9129c9f4d72334021a54f278373fae
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-26 22:04:32 -0300

    fix: baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://gateway.marvel.com/v1/public';
+const BASE_URL = 'https://gateway.marvel.com/v1/public';
 
 const api = axios.create({
   baseURL: BASE_URL,
